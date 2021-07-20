b9339bd0d1ca0f07178a8ad5538b1b6ed4da460e
AEMuto <antoine.marseaud@gmail.com>
2021-07-20 19:50:06 +0200

    #2 Validation pour les noms & l'email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,19 +8,23 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelector(".modal-btn");
+const modalBackground = document.querySelector(".bground");
 const modalContent = document.querySelector(".content");
+const modalBody = document.querySelector(".modal-body");
+const modalButton = document.querySelector(".modal-btn");
+
 const formData = document.querySelectorAll(".formData");
+const [firstName, lastName, email, birthDate, tournamentNumber, cities, terms] =
+  formData;
 
 // launch modal event
-modalBtn.addEventListener("click", toggleModal);
+modalButton.addEventListener("click", toggleModal);
 
 // launch modal form
 function toggleModal() {
-  modalbg.style.display === "block"
-    ? (modalbg.style.display = "none")
-    : (modalbg.style.display = "block");
+  modalBackground.style.display === "block"
+    ? (modalBackground.style.display = "none")
+    : (modalBackground.style.display = "block");
 }
 
 function toggleModalAnimation() {
@@ -35,5 +39,97 @@ document.querySelector("span.close").addEventListener("click", e => {
   modalContent.style.animationName = "modalclose";
   setTimeout(toggleModal, 700);
   setTimeout(toggleModalAnimation, 700);
-  // modalContent.style.animationName = "modalopen";
+});
+
+let outsideModal;
+
+modalBody.addEventListener("mouseleave", () => {
+  outsideModal = true;
+});
+modalBody.addEventListener("mouseenter", () => {
+  outsideModal = false;
+});
+
+modalBackground.addEventListener("click", e => {
+  if (outsideModal) {
+    e.stopPropagation();
+    modalContent.style.animationName = "modalclose";
+    setTimeout(toggleModal, 700);
+    setTimeout(toggleModalAnimation, 700);
+  }
+});
+
+// Form Control
+
+// document.querySelector('.formData').setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du nom.')
+// document.querySelector(".formData").setAttribute("data-error-visible", "true")
+
+function nameValidation(value) {
+  return /^[a-zA-Z]{2,}$/.test(value);
+}
+
+function emailValidation(value) {
+  return /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/.test(value);
+}
+
+function birthDateValidation(value) {
+  console.log(Date.parse(value));
+}
+
+let firstNameIsValid;
+firstName.querySelector(".text-control").addEventListener("change", event => {
+  if (nameValidation(event.target.value)) {
+    firstName.setAttribute("data-error-visible", "false");
+    firstNameIsValid = true;
+  } else {
+    firstName.setAttribute("data-error-visible", "true");
+    firstName.setAttribute(
+      "data-error",
+      "Veuillez entrer 2 caractères alphabétiques ou plus"
+    );
+  }
+});
+
+let lastNameIsValid;
+lastName.querySelector(".text-control").addEventListener("change", event => {
+  if (nameValidation(event.target.value)) {
+    lastName.setAttribute("data-error-visible", "false");
+    lastNameIsValid = true;
+  } else {
+    lastName.setAttribute("data-error-visible", "true");
+    lastName.setAttribute(
+      "data-error",
+      "Veuillez entrer 2 caractères alphabétiques ou plus"
+    );
+  }
+});
+
+let emailIsValid;
+email.querySelector(".text-control").addEventListener("change", event => {
+  if (emailValidation(event.target.value)) {
+    email.setAttribute("data-error-visible", "false");
+    emailIsValid = true;
+  } else {
+    email.setAttribute("data-error-visible", "true");
+    email.setAttribute(
+      "data-error",
+      "Veuillez entrer une adresse email valide"
+    );
+  }
+});
+
+let birthDateIsValid;
+birthDate.querySelector(".text-control").addEventListener("change", event => {
+  console.log(event.target.value, typeof event.target.value);
+  console.log(Date.parse(event.target.value));
+  if (event.target.value) {
+    birthDate.setAttribute("data-error-visible", "false");
+    birthDateIsValid = true;
+  } else {
+    birthDate.setAttribute("data-error-visible", "true");
+    birthDate.setAttribute(
+      "data-error",
+      "Vous devez entrer votre date de naissance"
+    );
+  }
 });
